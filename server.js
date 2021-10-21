24332c70c6b80d4d9b5e736f4890a570f7cb08ab
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-10-20 21:26:24 -0400

    Setup server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js by Jack Loveday
+
+// Import dependencies
+const express = require("express");
+const html = require("./api/html");
+const api = require("./api/api");
+
+// Setup Express Server
+const app = express();
+
+// Open Port for Heroku, otherwise default to 3001
+const PORT = process.env.PORT || 3001;
+
+// Setup access to Public folder
+app.use(express.static("public"));
+
+// Setup Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Setup proper routes
+app.use("/api", api);
+app.use("/", html);
+
+// Run the app on our Port
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
